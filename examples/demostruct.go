46b113029642936c5ac29b00c44d43e1f1710dfe
dave andersen <dave.andersen@gmail.com>
2012-04-04 21:56:24 -0400

    Array support

--- examples/demostruct.go
+++ examples/demostruct.go
@@ -3,4 +3,5 @@ package encodedemo
 type Demostruct struct {
 	A int64
 	B int32
+	C [4]int16
 }
--- src/binidl/binidl.go
+++ src/binidl/binidl.go
@@ -8,6 +8,7 @@ import (
 	//	"go/scanner"
 	//"go/printer"
 	"reflect"
+	"strconv"
 )
 
 type Binidl struct {
@@ -50,102 +51,114 @@ func c(f, tname, orderconvert string) {
 	fmt.Printf("\t%s = %s(binary.LittleEndian.%s(bs))\n", f, tname, orderconvert)
 }
 
-func unmarshalField(fname, tname, pred string) {
-	f := pred + "." + fname
+func unmarshalField(fname, tname string) {
 	switch tname {
 	case "int", "int64", "uint64":
 		r(8)
-		c(f, tname, "Uint64")
+		c(fname, tname, "Uint64")
 	case "int32", "uint32":
 		r(4)
-		c(f, tname, "Uint32")
+		c(fname, tname, "Uint32")
 	case "int16", "uint16":
 		r(2)
-		c(f, tname, "Uint16")
+		c(fname, tname, "Uint16")
 	case "int8", "uint8":
 		r(1)
-		fmt.Printf("\t%s = b[0]\n", f)
+		fmt.Printf("\t%s = b[0]\n", fname)
 	default:
-		fmt.Printf("\tUnmarshal%s(&%s, w)\n", tname, f)
+		fmt.Printf("\tUnmarshal%s(&%s, w)\n", tname, fname)
 	}
 }
 
 // silly.  This completely duplicates marshalContents.  Fix me, Dave.
-func unmarshalContents(ts *ast.TypeSpec, pred string) {
-	switch ts.Type.(type) {
-	case *ast.StructType:
-		st := ts.Type.(*ast.StructType)
-		for _, f := range st.Fields.List {
-			fname := f.Names[0].Name
-			switch f.Type.(type) {
-			case *ast.Ident:
-				t := f.Type.(*ast.Ident)
-				unmarshalField(fname, t.Name, pred)
-			case *ast.SelectorExpr:
-				se := f.Type.(*ast.SelectorExpr)
-				fmt.Printf("\t%s.Unmarshal%s(&%s, w)\n",
-					se.X, se.Sel.Name, pred+"."+fname)
-				
-			}
-
-			//fmt.Println("obj: ", t.Obj)
-			//fmt.Println("t: ", t)
-			//fmt.Println("Field ", fname, " (", f, ") type ", reflect.TypeOf(f.Type))
-		}
-	default:
-		panic("Don't know how to handle this type yet")
+func walkContents(st *ast.StructType, pred string, fn func(*ast.Field, string)) {
+	for _, f := range st.Fields.List {
+		fname := f.Names[0].Name
+		newpred := pred+"."+fname
+		fn(f, newpred)
 	}
 }
 
-func marshalField(fname, tname, pred string) {
-	f := pred + "." + fname
+func unmarshalOne(f *ast.Field, pred string) {
+	switch f.Type.(type) {
+	case *ast.Ident:
+		t := f.Type.(*ast.Ident)
+		unmarshalField(pred, t.Name)
+	case *ast.SelectorExpr:
+		se := f.Type.(*ast.SelectorExpr)
+		fmt.Printf("\t%s.Unmarshal%s(&%s, w)\n",
+			se.X, se.Sel.Name, pred)
+	case *ast.ArrayType:
+		s := f.Type.(*ast.ArrayType)
+		e, ok := s.Len.(*ast.BasicLit)
+		if !ok {
+			panic("Bad literal in array decl")
+		}
+		
+		len, _ := strconv.Atoi(e.Value) // check the error, lazybones
+		//fmt.Println("Array len: ", len)
+		for i := 0; i < len; i++ {
+			fsub := fmt.Sprintf("%s[%d]", pred, i)
+			pseudofield := &ast.Field{nil, nil, s.Elt, nil, nil}
+			//fmt.Println("PF: ", pseudofield)
+			unmarshalOne(pseudofield, fsub)
+			//fmt.Println("Type of elt: ", reflect.TypeOf(s.Elt))
+			//unmarshalContents(s.Elt
+			//unmarshalField(fsub, elt.Name, pred)
+		}
+	default:
+		panic("Unknown type in struct")
+	}
+}
+
+func marshalField(fname, tname string) {
 	switch tname {
 	case "int", "int64", "uint64":
 		b(8)
-		bs("binary.LittleEndian.PutUint64", "uint64", f)
+		bs("binary.LittleEndian.PutUint64", "uint64", fname)
 		wbs()
 	case "int32", "uint32":
 		b(4)
-		bs("binary.LittleEndian.PutUint32", "uint32", f)
+		bs("binary.LittleEndian.PutUint32", "uint32", fname)
 		wbs()
 	case "int16", "uint16":
 		b(2)
-		bs("binary.LittleEndian.PutUint16", "uint16", f)
+		bs("binary.LittleEndian.PutUint16", "uint16", fname)
 		wbs()
 	case "int8", "uint8":
 		b(1)
-		fmt.Printf("\tb[0] = byte(%s)\n", f)
+		fmt.Printf("\tb[0] = byte(%s)\n", fname)
 		wbs()
 	default:
-		fmt.Printf("\tMarshal%s(&%s, w)\n", tname, f)
+		fmt.Printf("\tMarshal%s(&%s, w)\n", tname, fname)
 	}
 }
 
-func marshalContents(ts *ast.TypeSpec, pred string) {
-	//fmt.Println("Marshaling ", ts, " with ", pred)
-	switch ts.Type.(type) {
-	case *ast.StructType:
-		st := ts.Type.(*ast.StructType)
-		//fmt.Println("Hey, a struct.  Recurse on its contents.")
-		for _, f := range st.Fields.List {
-			fname := f.Names[0].Name
-			switch f.Type.(type) {
-			case *ast.Ident:
-				t := f.Type.(*ast.Ident)
-				marshalField(fname, t.Name, pred)
-			case *ast.SelectorExpr:
-				se := f.Type.(*ast.SelectorExpr)
-				fmt.Printf("\t%s.Marshal%s(&%s, w)\n",
-					se.X, se.Sel.Name, pred+"."+fname)
-				
-			}
 
-			//fmt.Println("obj: ", t.Obj)
-			//fmt.Println("t: ", t)
-			//fmt.Println("Field ", fname, " (", f, ") type ", reflect.TypeOf(f.Type))
+func marshalOne(f *ast.Field, pred string) {
+	switch f.Type.(type) {
+	case *ast.Ident:
+		t := f.Type.(*ast.Ident)
+		marshalField(pred, t.Name)
+	case *ast.SelectorExpr:
+		se := f.Type.(*ast.SelectorExpr)
+		fmt.Printf("\t%s.Marshal%s(&%s, w)\n",
+			se.X, se.Sel.Name, pred)
+	case *ast.ArrayType:
+		s := f.Type.(*ast.ArrayType)
+		e, ok := s.Len.(*ast.BasicLit)
+		if !ok {
+			panic("Bad literal in array decl")
+		}
+		
+		len, _ := strconv.Atoi(e.Value) // check the error, lazybones
+		for i := 0; i < len; i++ {
+			fsub := fmt.Sprintf("%s[%d]", pred, i)
+			pseudofield := &ast.Field{nil, nil, s.Elt, nil, nil}
+			marshalOne(pseudofield, fsub)
 		}
 	default:
-		panic("Don't know how to handle this type yet")
+		panic("Unknown type in struct")
 	}
 }
 
@@ -161,18 +174,23 @@ func structmap(n interface{}) {
 	//fmt.Println("Got a type!")
 	ts := decl.Specs[0].(*ast.TypeSpec)
 	typeName := ts.Name.Name
+	st, ok := ts.Type.(*ast.StructType)
+	if !ok {
+		return
+	}
+
 	//fmt.Println("ts: ", typeName)
 	fmt.Printf("func Marshal%s(t *%s, w io.Writer) {\n", typeName, typeName)
 	fmt.Println("\tvar b [8]byte")
 	fmt.Println("\tbs := b[:8]")
 	//fmt.Printf("tstype: ", reflect.TypeOf(ts.Type))
-	marshalContents(ts, "t")
+	walkContents(st, "t", marshalOne)
 	fmt.Println("}\n")
 
 	fmt.Printf("func Unmarshal%s(t *%s, r io.Reader) error {\n", typeName, typeName)
 	fmt.Println("\tvar b [8]byte")
 	fmt.Println("\tvar bs []byte")
-	unmarshalContents(ts, "t")
+	walkContents(st, "t", unmarshalOne)
 	fmt.Println("\treturn nil\n}\n")
 	return
 }
