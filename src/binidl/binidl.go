35256e1dfce541ed984f62034ef4bce104df0970
dave andersen <dave.andersen@gmail.com>
2012-04-06 22:54:33 -0400

    First try at optimizing statically sized data structures.  Probably fragile and reveals a design flaw with handling array for loops.  But works for small ones.

--- src/binidl/binidl.go
+++ src/binidl/binidl.go
@@ -30,29 +30,24 @@ func NewBinidl(filename string) *Binidl {
 }
 
 var cur_b_marshal_state int = 0
-var first_blen int = 0
 
-func setbs(b io.Writer, n int) {
-	if first_blen == 0 {
-		first_blen = n
-	} else {
-		if n != cur_b_marshal_state {
-			fmt.Fprintf(b, "bs = b[:%d]\n", n)
-		}
+func setbs(b io.Writer, n int, es *EmitState) {
+	if (es.isStatic) {
+		return
+	}
+	if n != cur_b_marshal_state {
+		fmt.Fprintf(b, "bs = b[:%d]\n", n)
 	}
 	cur_b_marshal_state = n
 }
-func bs(b io.Writer, fname, tname, orderconvert string) {
-	fmt.Fprintf(b, "%s(bs, %s(%s))\n", orderconvert, tname, fname)
-}
 
-func r(b io.Writer, n int) {
-	setbs(b, n)
+func r(b io.Writer, n int, es *EmitState) {
+	setbs(b, n, es)
 	fmt.Fprintf(b, "if _, err := io.ReadFull(r, bs); err != nil {\n")
 	fmt.Fprintf(b, "return err\n}\n")
 }
 
-func unmarshalField(b io.Writer, fname, tname string) {
+func unmarshalField(b io.Writer, fname, tname string, es *EmitState) {
 	tconv := tname
 	if mapped, ok := typemap[tname]; ok {
 		tconv = mapped
@@ -64,7 +59,7 @@ func unmarshalField(b io.Writer, fname, tname string) {
 		return
 	}
 
-	r(b, ti.Size)
+	r(b, ti.Size, es)
 	if ti.Size == 1 {
 		fmt.Fprintf(b, "%s = %s(b[0])\n", fname, tname)
 	} else {
@@ -72,7 +67,7 @@ func unmarshalField(b io.Writer, fname, tname string) {
 	}
 }
 
-func marshalField(b io.Writer, fname, tname string) {
+func marshalField(b io.Writer, fname, tname string, es *EmitState) {
 	if mapped, ok := typemap[tname]; ok {
 		tname = mapped
 	}
@@ -83,39 +78,57 @@ func marshalField(b io.Writer, fname, tname string) {
 		return
 	}
 
-	setbs(b, ti.Size)
+	setbs(b, ti.Size, es)
 	if ti.Size == 1 {
-		fmt.Fprintf(b, "b[0] = byte(%s)\n", fname)
+		fmt.Fprintf(b, "b[%d] = byte(%s)\n", es.Bstart(1), fname)
 	} else {
-		bs(b, fname, ti.EncodesAs, encodeFunc[ti.EncodesAs])
+		ef := encodeFunc[ti.EncodesAs]
+		if !es.isStatic {
+			fmt.Fprintf(b, "%s(bs, %s(%s))\n", ef, ti.EncodesAs, fname)
+		} else {
+			bstart := es.Bstart(ti.Size)
+			bend := bstart + ti.Size
+			fmt.Fprintf(b, "%s(b[%d:%d], %s(%s))\n", ef, bstart, bend, ti.EncodesAs, fname)
+		}
 	}
-	fmt.Fprintf(b, "w.Write(bs)\n")
+	if !es.isStatic {
+		fmt.Fprintf(b, "w.Write(bs)\n")
+	}
 }
 
-func walkContents(b io.Writer, st *ast.StructType, pred string, funcname string, fn func(io.Writer, string, string)) {
+func walkContents(b io.Writer, st *ast.StructType, pred string, funcname string, fn func(io.Writer, string, string, *EmitState), es *EmitState) {
 	for _, f := range st.Fields.List {
 		fname := f.Names[0].Name
 		newpred := pred + "." + fname
-		walkOne(b, f, newpred, funcname, fn)
+		walkOne(b, f, newpred, funcname, fn, es)
 	}
 }
 
 var index_depth int = 0
 
-func get_index_str() string {
+func (es *EmitState) getIndexStr() string {
 	indexes := []string{"i", "j", "k", "ii", "jj", "kk"}
-	if index_depth > 5 {
+	if es.nextIdx > 5 {
 		panic("Array nesting depth too large.  Lazy programmer bites again.")
 	}
-	i := index_depth
-	index_depth++
+	i := es.nextIdx
+	es.nextIdx++
 	return indexes[i]
 }
-func free_index_str() {
-	index_depth--
+
+func (es *EmitState) freeIndexStr() {
+	es.nextIdx--
 }
 
-var need_readbyte bool = false
+func (es *EmitState) Bstart(n int) int {
+	if !es.isStatic { 
+		return 0
+	}
+	o := es.staticOffset
+	es.staticOffset += n
+	return o
+}
+
 var need_bufio = false
 
 var typemap map[string]string = make(map[string]string)
@@ -151,15 +164,15 @@ var typedb map[string]TypeInfo = map[string]TypeInfo{
 	"byte":   {"byte", 1, "byte"},
 }
 
-func walkOne(b io.Writer, f *ast.Field, pred string, funcname string, fn func(io.Writer, string, string)) {
+func walkOne(b io.Writer, f *ast.Field, pred string, funcname string, fn func(io.Writer, string, string, *EmitState), es *EmitState) {
 	ioid := "w"
-	if funcname == "Unmarshal" {
+	if es.op == UNMARSHAL {
 		ioid = "r"
 	}
 	switch f.Type.(type) {
 	case *ast.Ident:
 		t := f.Type.(*ast.Ident)
-		fn(b, pred, t.Name)
+		fn(b, pred, t.Name, es)
 	case *ast.SelectorExpr:
 		//se := f.Type.(*ast.SelectorExpr)
 		//fmt.Printf("%s.%s%s(&%s, w)\n",
@@ -168,12 +181,11 @@ func walkOne(b io.Writer, f *ast.Field, pred string, funcname string, fn func(io
 			pred, funcname, ioid)
 	case *ast.ArrayType:
 		s := f.Type.(*ast.ArrayType)
-		i := get_index_str()
+		i := es.getIndexStr()
 		fmt.Fprintf(b, "{\n")
+		arrayLen := 0
 		if s.Len == nil {
 			// If we are unmarshaling we need to allocate.
-			need_readbyte = true
-			need_bufio = true
 			if ioid == "r" {
 				fmt.Fprintf(b, "len, err := binary.ReadVarint(r)\n")
 				fmt.Fprintf(b, "if err != nil {\n")
@@ -181,7 +193,7 @@ func walkOne(b io.Writer, f *ast.Field, pred string, funcname string, fn func(io
 				fmt.Fprintf(b, "}\n")
 				fmt.Fprintf(b, "%s = make([]%s, len)\n", pred, s.Elt)
 			} else {
-				setbs(b, 10)
+				setbs(b, 10, es)
 				fmt.Fprintf(b, "len := int64(len(%s))\n", pred)
 				fmt.Fprintf(b, "if wlen := binary.PutVarint(bs, len); wlen >= 0 {\n")
 				fmt.Fprintf(b, "w.Write(b[0:wlen])\n")
@@ -193,20 +205,134 @@ func walkOne(b io.Writer, f *ast.Field, pred string, funcname string, fn func(io
 			if !ok {
 				panic("Bad literal in array decl")
 			}
-			len, err := strconv.Atoi(e.Value)
+			var err error
+			arrayLen, err = strconv.Atoi(e.Value)
 			if err != nil {
 				panic("Bad array length value.  Must be a simple int.")
 			}
-			fmt.Fprintf(b, "for %s := 0; %s < %d; %s++ {\n", i, i, len, i)
+			if !es.isStatic {
+				fmt.Fprintf(b, "for %s := 0; %s < %d; %s++ {\n", i, i, arrayLen, i)
+			}
 		}
 
 		fsub := fmt.Sprintf("%s[%s]", pred, i)
 		pseudofield := &ast.Field{nil, nil, s.Elt, nil, nil}
-		walkOne(b, pseudofield, fsub, funcname, fn)
-		fmt.Fprintf(b, "}\n}\n")
-		free_index_str()
+		if es.isStatic {
+			for idx := 0; idx < arrayLen; idx++ {
+				fsub := fmt.Sprintf("%s[%d]", pred, idx)
+				walkOne(b, pseudofield, fsub, funcname, fn, es)
+			}
+		} else {
+			walkOne(b, pseudofield, fsub, funcname, fn, es)
+			fmt.Fprintln(b, "}")
+		}
+		fmt.Fprintln(b, "}")
+		es.freeIndexStr()
 	default:
 		panic("Unknown type in struct")
+	}
+}
+
+type StructInfo struct {
+	size int
+	maxSize int
+	firstSize int
+	varLen  bool
+	mustDispatch bool
+}
+const (
+	MARSHAL = iota
+	UNMARSHAL
+)
+
+type EmitState struct {
+	op int // 0 = marshal, 1 = unmarshal
+	nextIdx int // 0 = 'i', 1 = 'j', ..., 'z'
+	isStatic bool
+	staticOffset int
+}
+
+func mergeInfo(parent, child *StructInfo, childcount int) {
+	if parent.firstSize == 0 {
+		parent.firstSize = child.firstSize
+	}
+	if child.maxSize > parent.maxSize {
+		parent.maxSize = child.maxSize
+	}
+	if child.varLen {
+		parent.varLen = true
+	}
+	if child.mustDispatch {
+		parent.mustDispatch = true
+	}
+	if childcount > 0 {
+		parent.size += child.size * childcount
+	}
+	
+}
+
+// Wouldn't it be nice to cache a lot of this? :-)
+func analyze(info *StructInfo, n interface{}) {
+	switch n.(type) {
+	case  *ast.StructType:
+		st := n.(*ast.StructType)
+		for _, field := range st.Fields.List {
+			subinfo := &StructInfo{}
+			analyze(subinfo, field)
+			mergeInfo(info, subinfo, 1)
+		}
+	case *ast.Field:
+		f := n.(*ast.Field)
+		switch(f.Type.(type)) {
+		case *ast.Ident:
+			t := f.Type.(*ast.Ident)
+			tname := t.Name
+			if mapped, ok := typemap[tname]; ok {
+				tname = mapped
+			}
+			if tinfo, ok := typedb[tname]; ok {
+				if info.firstSize == 0 {
+					info.firstSize = tinfo.Size
+				}
+				if tinfo.Size > info.maxSize {
+					info.maxSize = tinfo.Size
+				}
+				info.size += tinfo.Size
+			} else {
+				info.mustDispatch = true
+			}
+			return
+		case *ast.SelectorExpr:
+			info.mustDispatch = true
+			return
+		case *ast.ArrayType:
+			s := f.Type.(*ast.ArrayType)
+			arraylen := 0
+			if s.Len == nil {
+				// If we are unmarshaling we need to allocate.
+				info.varLen = true
+				need_bufio = true // eventually just in info
+			} else {
+				e, ok := s.Len.(*ast.BasicLit)
+				if !ok {
+					panic("Bad literal in array decl")
+				}
+				var err error
+				arraylen, err = strconv.Atoi(e.Value)
+				if err != nil {
+					panic("Bad array length value.  Must be a simple int.")
+				}
+			}
+			
+			subinfo := &StructInfo{}
+			pseudofield := &ast.Field{nil, nil, s.Elt, nil, nil}
+			analyze(subinfo, pseudofield)
+			mergeInfo(info, subinfo, arraylen)
+		default:
+			panic("Unknown type in struct")
+		}
+	default:
+		panic("Unknown ast type")
 	}
 }
 
@@ -234,44 +360,66 @@ func structmap(out io.Writer, n interface{}) {
 		}
 		panic("Can't handle decl!")
 	}
+	info := &StructInfo{}
+	analyze(info, st)
+	//fmt.Println("Analysis result: ", info)
+
 
 	b := new(bytes.Buffer)
 
-	need_readbyte = false
-	first_blen = 0
 	//fmt.Println("ts: ", typeName)
-	walkContents(b, st, "t", "Marshal", marshalField)
+	mes := &EmitState{}
+	mes.op = MARSHAL
+	if (info.size < 64 && !info.varLen && !info.mustDispatch) {
+		mes.isStatic = true
+		fmt.Fprintf(os.Stderr, "Hey!  I'm trying static\n")
+	}
+	walkContents(b, st, "t", "Marshal", marshalField, mes)
 	blen := 8
-	if need_readbyte {
+	if info.varLen {
 		blen = 10
+	}
+	if mes.isStatic {
+		blen = info.size
 	}
 	fmt.Fprintf(out, "func (t *%s) Marshal(w io.Writer) {\n", typeName)
 	fmt.Fprintf(out, "var b [%d]byte\n", blen)
-	fmt.Fprintf(out, "bs := b[:%d]\n", first_blen)
+	if !mes.isStatic {
+		fmt.Fprintf(out, "bs := b[:%d]\n", info.firstSize)
+	}
+	cur_b_marshal_state = info.firstSize
 
 	b.WriteTo(out)
+	if mes.isStatic {
+		fmt.Fprintln(out, "w.Write(b[:])")
+	}
 	fmt.Fprintln(out, "}\n")
 
 	b.Reset()
-	first_blen = 0
-	walkContents(b, st, "t", "Unmarshal", unmarshalField)
+	ues := &EmitState{}
+	ues.op = UNMARSHAL
+	blen = 8
+	if info.varLen {
+		blen = 10
+	}
+	walkContents(b, st, "t", "Unmarshal", unmarshalField, ues)
 	paramname := "r"
-	if need_readbyte {
+	if info.varLen {
 		paramname = "rr"
 	}
 	fmt.Fprintf(out, "func (t *%s) Unmarshal(%s io.Reader) error {\n", typeName, paramname)
 	
-	if need_readbyte {
-		fmt.Fprintln(out, `
-var r byteReader
+	if info.varLen {
+		fmt.Fprintln(out, 
+`var r byteReader
 var ok bool
 if r, ok = rr.(byteReader); !ok {
     r = bufio.NewReader(rr)
 }`)
 	}
 	fmt.Fprintf(out, "var b [%d]byte\n", blen)
-	fmt.Fprintf(out, "bs := b[:%d]\n", first_blen)
-	cur_b_marshal_state = first_blen
+	fmt.Fprintf(out, "bs := b[:%d]\n", info.firstSize)
+	cur_b_marshal_state = info.firstSize
 	b.WriteTo(out)
 	fmt.Fprintln(out, "return nil\n}\n")
 	return
